9d0673535e8218ac7030f35d635f632831800a4a
Thunderzapper <justinabossmlg@gmail.com>
2022-04-10 18:35:49 -0400

    linux: clone the linux branches

--- linux/installer.sh
+++ linux/installer.sh
@@ -79,7 +79,7 @@ elif [ $Choice = '1' ]
 then
 	clear
 	echo 'Cloning repository from GitHub...'
-	git clone https://github.com/Wrapper-Offline/Wrapper-Offline.git
+	git clone --single-branch --branch linux ffhttps://github.com/Wrapper-Offline/Wrapper-Offline.git
 	clear
 	echo 'Wrapper: Offline has been installed! Feel free to move it wherever you want.'
 	xdg-open ./Wrapper-Offline
@@ -87,7 +87,7 @@ elif [ $Choice = '2' ]
 then
 	clear
 	echo 'Cloning repository from GitHub...'
-	git clone --single-branch --branch beta https://github.com/Wrapper-Offline/Wrapper-Offline.git
+	git clone --single-branch --branch beta-linux https://github.com/Wrapper-Offline/Wrapper-Offline.git
 	clear
 	echo 'Wrapper: Offline has been installed! Feel free to move it wherever you want.'
 	xdg-open ./Wrapper-Offline
